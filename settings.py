94c9b4009499020d7b34fce42003058ee18fe88b
溝渕正利 <jo-jaku@M1.local>
2024-12-03 16:06:27 +0900

    Initial commit

--- settings.py
+++ settings.py
@@ -110,11 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 静的ファイルを収集するディレクトリ
-STATICFILES_DIRS = [BASE_DIR / "static"]  # 修正
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / 'search_app' / 'static']
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
